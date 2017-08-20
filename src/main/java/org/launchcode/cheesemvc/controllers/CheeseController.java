48e725ea76fc09edfa9a3aa98d2377309202364e
John Wallace <johncwallace@gmail.com>
2017-08-20 14:23:19 -0700

    My work thus far

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,54 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.HashMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("cheese")
+
+public class CheeseController {
+    static ArrayList<String> cheese;
+    static HashMap <String, String> cheeses = new HashMap<>();
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
+        // Redirect to /cheese
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "Cheese to Remove");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(Model model, @RequestParam("passedCheeses") List<String> cheeseAway) {
+        return "redirect:";
+    }
+}
